69507f7932b58539f43147b1b3a14635ef571ed6
jittawee <jtwe.esr@gmail.com>
2025-09-22 22:15:36 +0700

    fix cors origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:5175",
-    "https://obsidian-sip.vercel.app",
+    "https://obsidian-sip-dun.vercel.app",
   ],
   credentials: true,
 };
