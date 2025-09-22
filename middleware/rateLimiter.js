09c0a68cdf7aebc2e467fcb73267b86c7708f3b3
jittawee <jtwe.esr@gmail.com>
2025-09-22 22:16:43 +0700

    fix limiter

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 500, // limit each IP to 100 requests
+  max: 100000, // limit each IP to 100000 requests
   standardHeaders: true,
   legacyHeaders: false,
 });
